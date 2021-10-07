d65a7614e1e16ec9ab73ce5a65ff36475e4f6a22
Troshin Sergey <serge-troshin@yandex.ru>
2021-10-07 16:35:21 +0300

    commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/* libs */
+
+//= libs/jquery-3.3.1.min.js
+//= libs/jquery.inputmask.min.js
+//= libs/select2.full.min.js
+
+/* my scripts */
+
+//= partials/main.js
---
+++ src/js/partials/main.js
@@ -0,0 +1,150 @@
+$(document).ready(function () {
+    if ($('.input-block').length > 0){
+        $('.input-block input, .input-block textarea').each(function () {
+            if ($(this).val() != '') $(this).addClass('not-empty');
+        });
+        $('.input-block input, .input-block textarea').on('change keyup paste',function () {
+            if ($(this).val() != '') {
+                $(this).addClass('not-empty');
+            } else {
+                $(this).removeClass('not-empty')
+            }
+        });
+    }
+
+    $('input[data-group]').on('change keyup paste',function () {
+        var group = $(this).data('group');
+        if ($(this).val() != '') {
+            $('input[data-group="'+group+'"]').not(this).prop('disabled',true);
+        } else {
+            $('input[data-group="'+group+'"]').not(this).prop('disabled',false);
+        }
+    });
+
+    $('.input-block__password-toggle').click(function () {
+        var input = $(this).siblings('input');
+        if (input.attr('type') === 'password') {
+            input.attr('type', 'text');
+        } else {
+            input.attr('type', 'password');
+        }
+    });
+
+
+
+    $('.orders-table__row--item .checkbox input').on('change',function () {
+        if ($(this).is(":checked")){
+            $(this).parents('.orders-table__row--item').addClass('active');
+        } else {
+            $(this).parents('.orders-table__row--item').removeClass('active');
+        }
+    });
+
+    $('.check-all input').on('change',function () {
+        if ($(this).is(":checked")){
+            $('.orders-table__row--item').addClass('active');
+            $('.orders-table__row--item .checkbox input').prop( "checked", true );
+        } else {
+            $('.orders-table__row--item').removeClass('active');
+            $('.orders-table__row--item .checkbox input').prop( "checked", false );
+        }
+    });
+    /*$('.orders-table__counter').click(function (event) {
+        event.preventDefault();
+        $(this).parents('.orders-table__row--item').next().slideToggle('',function () {
+            $(this).toggleClass('active');
+        });
+    });*/
+    
+    $('.orders-table__row--item').click(function (event) {
+        event.preventDefault();
+        $(this).next().slideToggle('',function () {
+            $(this).toggleClass('active');
+        });
+        $(this).find('.orders-table__toggle').toggleClass('active');
+    });
+
+    $('.topbar__menu').click(function () {
+        $('.sidebar').addClass('active');
+        $('.bgoverflow').addClass('active');
+    });
+    $('.bgoverflow').click(function () {
+        $('.sidebar').removeClass('active');
+        $('.bgoverflow').removeClass('active');
+    });
+
+    $('.mask-vin').inputmask({ "mask": "* ", "repeat": 20, "greedy": true });
+    $('.mask-rub').inputmask({ "mask": "9{1,10} руб.",  "greedy": false, "placeholder": "0" });
+    $('.mask-phone').inputmask({ "mask": "+7 (999) 999-99-99",  "greedy": true });
+
+
+    function formatContact (contact) {
+        if (!contact.id) {
+            return contact.text;
+        }
+        var baseUrl = "/img";
+        var $contact = $(
+            '<span><img src="' + baseUrl + '/' + contact.element.dataset.img + '" class="img-contact" /> ' + contact.text + '</span>'
+        );
+        return $contact;
+    };
+
+
+    function formatSelection (contact) {
+        if (!contact.id) {
+            return contact.text;
+        }
+
+        var baseUrl = "/img";
+        var $state = $(
+            '<span><img class="img-contact" /> <span></span></span>'
+        );
+
+        $state.find("span").text(contact.text);
+        $state.find("img").attr("src", baseUrl + "/" + contact.element.dataset.img);
+
+        return $state;
+    };
+
+    $(".js-contact-select").select2({
+        templateResult: formatContact,
+        templateSelection: formatSelection,
+        language: "ru"
+    });
+
+    $('.js-select').select2({
+        //templateResult: formatContact,
+        //templateSelection: formatSelection,
+        language: "ru"
+    });
+
+    //
+    const inputHints = document.querySelectorAll('.input-block__hints button');
+
+    if (inputHints.length) {
+        inputHints.forEach(it => {
+            it.addEventListener('click', (e) => {
+                const input = it.parentElement.parentElement.querySelector('input');
+                input.value = it.textContent;
+                input.classList.add('not-empty');
+            })
+        })
+    }
+
+    //Мои заказы аккордеон
+    /*const orderToggleBtns = document.querySelectorAll('.orders-table__toggle');
+
+    if (orderToggleBtns.length) {
+        orderToggleBtns.forEach(it => {
+            it.onclick = () => {
+                if (it.classList.contains('active')) {
+                    it.classList.remove('active');
+                    it.parentElement.nextElementSibling.classList.remove('active');
+                } else {
+                    it.classList.add('active');
+                    it.parentElement.nextElementSibling.classList.add('active');
+                }
+            }
+        })
+    }*/
+});
